0fea2ede5274e7f5ec430e22610594963e01e433
Osgen <ovirgen0@ucol.mx>
2019-08-16 16:26:33 -0500

    3rd commit - Refactor router taskController->Ready

--- app/controllers/taskController.js
+++ app/controllers/taskController.js
@@ -1,20 +1,18 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send("task root get");
-});
-
-router.post('/',(req,res)=>{
+router.route('/')
+    .get((req,res)=>{
+        res.send("task root get");
+    })
+    .post((req,res)=>{
     res.send("task root post");
-});
-
-router.put('/',(req,res)=>{
-    res.send("task root put");
-});
-
-router.delete('/',(req,res)=>{
-    res.send("task root delete");
+    })
+    .put((req,res)=>{
+        res.send("task root put");
+    })
+    .delete((req,res)=>{
+        res.send("task root delete");
 });
 
 module.exports = router;
