7758abd47a478241405841e6d0ddca071f9cb9bd
0scar Vc <ovirgen0@ucol.mx>
2019-08-18 15:55:38 -0500

    14th commit -m Add bodyParser to server -> Ready

--- app/appController.js
+++ app/appController.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const task = require('./controllers/taskController');//gets all the routes for 'task'
+const parser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;//sets PORT
 
-app.use('/task', task);//sets for what route use the 'task' routing
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+app.use('/tasks', task);//sets for what route use the 'task' routing
 
 
-app.listen(PORT);
+app.listen(PORT, ()=>{"Yo Server is ON!"});
