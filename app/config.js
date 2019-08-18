e882d4d8155d7786308eb22dd6076e87e42e5d3c
0scar Vc <ovirgen0@ucol.mx>
2019-08-18 15:53:41 -0500

    12th commit -m Connect to hosted mongobd ->Ready

--- app/config.js
+++ app/config.js
@@ -3,6 +3,10 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/auth');
+mongoose.connect('mongodb://js:hipermedia2019@ds263307.mlab.com:63307/hipermedia2019', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  // eslint-disable-next-line
+  console.log('conneted to database');
+});
 
 module.exports = mongoose;
