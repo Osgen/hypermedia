065b92890247c805add07ab991769e872834121e
visita <visita@TELEMATICA.UCOL.MX>
2019-08-30 08:21:48 -0500

    Oauth added

--- app/appController.js
+++ app/appController.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req, res)=>{
-    res.send("AVER QUE PEDO lco");
-});
+// app.get('/',(req, res)=>{
+//     res.send("AVER QUE PEDO lco");
+// });
 
 
 app.listen(PORT);
