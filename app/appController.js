fac70eea688f5eab74579363c4429f9af8d86f02
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-09-06 08:30:16 -0500

    4th commit - Get google user data and refactor ->Ready

--- app/appController.js
+++ app/appController.js
@@ -1,24 +1,9 @@
+require('./services/passport');
 const express = require('express');
-const passport = require('passport');
-const googleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-
 const app = express();
 
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-passport.use(new googleStrategy({
-
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-
-}, (accessToken) => {
-    console.log(accessToken);
-}));
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
+require('./routes/authRoutes')(app);
 
 app.listen(port, () => console.log(`Escuchando en el puerto ${port}`));
