6ae59ab5e6fa2c9057d5ff1ada49c5ae30bd3f3d
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-09-25 08:31:10 -0500

    7th Commit - Add production section ->Ready

---
+++ app/config/keys.js
@@ -0,0 +1,8 @@
+// Say which credentials to return
+if(process.env.NODE_ENV === 'production'){
+    // We are in production
+    module.exports = require('./prod');
+}else{
+    // We are in development
+    module.exports = require('./dev');
+}
