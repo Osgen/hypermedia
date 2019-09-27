5c279cc4494ee3ce3ef94837b596c8a840e27549
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-09-27 08:36:08 -0500

    9th Commit - add react ->Ready

--- app/appController.js
+++ app/appController.js
@@ -5,7 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const keys = require('./config/keys');
 
 app.use(
