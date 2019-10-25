2893fc5cd994c5ed20fca7385f760ca77f8481a1
20136386 OSCAR VIRGEN CASILLAS <20136386@TELEMATICA.UCOL.MX>
2019-10-25 08:29:56 -0500

    Client - add redux, refact src folder and add authReducer ->Ready

--- app/appController.js
+++ app/appController.js
@@ -8,14 +8,6 @@ const passport = require('passport');
 const port = process.env.PORT || 5000;
 const keys = require('./config/keys');
 
-// const proxy = require('./client/node_modules/http-proxy-middleware');
-
-// app.use(proxy('/auth/google',{target:'http://localhost:5000'}));
-// app.use(proxy('/auth/google/callback',{target:'http://localhost:5000'}));
-// app.use(proxy('/api/current_user',{target:'http://localhost:5000'}));
-
-// app.use(proxy('/api/**',{target:'http://localhost:5000'}));
-
 app.use(
     cookieSession({
         maxAge:30*24*60*60*1000,
